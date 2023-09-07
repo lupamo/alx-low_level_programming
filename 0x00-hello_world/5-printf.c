5fa1e5ce845e26d2f8e9b20b218278cbf3a0e210
lupamo <arnoldlupamo@gmail.com>
2023-09-07 13:34:13 -0700

    print f554

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-/**
- *main-str
- *Return: integer after executing
-*/
+/*
+ * main:prints str output
+ * return - main return 0 when executed
+ */
 #include <stdio.h>
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
