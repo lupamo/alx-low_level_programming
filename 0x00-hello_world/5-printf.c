9d37307c39a5656067ff8545cde1ec28790bb5e9
lupamo <arnoldlupamo@gmail.com>
2023-09-07 14:23:49 -0700

    print f6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
-/*
- *main-Prints with proper grammar, but the oucome is a piece of art, 
- *Return:zero after main is executed 
- */
 #include <stdio.h>
+/**
+ * main - printing a str
+ *
+ * Return: zero means code success
+ */ 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
