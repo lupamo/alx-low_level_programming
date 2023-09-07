f06b0d7451c2709d2bc8a942d8ef43c367db6fb5
lupamo <arnoldlupamo@gmail.com>
2023-09-07 12:45:46 -0700

    outputting a line7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 /**
- * This program demonstrates the use of the puts function to print a message to the console.
  *
- * @return 0 indicates successful execution.
+ * @return indicates successful execution.
  */
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*return value for main*/
 	return (0);
 }
