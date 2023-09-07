1f1b81d6e75a640c8cd7b169bd3bb47d61a07d71
lupamo <arnoldlupamo@gmail.com>
2023-09-07 15:26:49 -0700

    now7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, sizeof(msg) - 1);
 	return (1);
 }
