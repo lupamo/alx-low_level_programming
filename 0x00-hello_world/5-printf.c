f8bcad938cbc60e1a6108a70d71b6a517b513d9a
lupamo <arnoldlupamo@gmail.com>
2023-09-07 13:12:26 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ *main- prints a sentence
+ *return- returns an integer
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
