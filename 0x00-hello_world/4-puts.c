517b4b0e4e237a6172d0cd074d48aeed144d9862
lupamo <arnoldlupamo@gmail.com>
2023-09-07 11:57:28 -0700

    outputting a line3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int
 /**
  * my_function - This is a description
  */
-main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
