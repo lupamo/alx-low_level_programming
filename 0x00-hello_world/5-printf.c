99e6c15258e1a29dbebc63015d92118a9803f38f
lupamo <arnoldlupamo@gmail.com>
2023-09-07 13:15:35 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 /**
  *main- prints a sentence
- *return- returns an integer
+ *return: returns an integer
  */
 #include <stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
