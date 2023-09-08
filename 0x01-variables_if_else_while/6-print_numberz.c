4e4396ca29562a41b9413972d94f3ee335cc00f0
lupamo <arnoldlupamo@gmail.com>
2023-09-08 12:27:39 -0700

    zero foward

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - basse numbers
+ *
+ * Return: 0 after successful executing
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+
+	return (0);
+}
