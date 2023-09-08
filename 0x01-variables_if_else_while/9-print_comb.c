cb42abc34983c519e90553bca61eb842b4769f85
lupamo <arnoldlupamo@gmail.com>
2023-09-08 13:14:47 -0700

    what

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,22 +6,15 @@
  */
 int main(void)
 {
-	int k, l;
-
-	for (k = 0; k <= 9; k++)
+	int i;
+	for (i = 0; i <= 9; i++)
 	{
-		for (l = k + 1; l <= 9; l++)
+		putchar('0' + i);
+
+		if (i < 9)
 		{
-			putchar('0' + k);
 			putchar(',');
 			putchar(' ');
-			putchar('0' + l);
-
-			if (k !=8 || l != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
