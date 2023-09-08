2c5726084500de6fe26950871f07b932b07b062f
lupamo <arnoldlupamo@gmail.com>
2023-09-08 11:55:00 -0700

    miss e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - all letters except q and e
+ *
+ * Return: 0 after success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != e && letter != q)
+		{
+			putchar(letter);
+		}
+
+		letter++;
+	}
+	putchar('\n');
+	
+	return(0);
+
+}
