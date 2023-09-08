af805c4fd449e21b33a866cd07fe47785632d134
lupamo <arnoldlupamo@gmail.com>
2023-09-08 12:36:42 -0700

    reverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ *
+ * Return: 0 after success
+ */
+int main(void)
+{
+	char letter = 'z';
+	while(letter >= 'a')
+	{
+		putchar(letter);
+		letter--;		
+	}
+	putchar("\n");
+
+	return (0);
+}
