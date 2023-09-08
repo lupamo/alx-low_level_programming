d3ce879c44fb70cd685575dcd28aa4bc0519e66e
lupamo <arnoldlupamo@gmail.com>
2023-09-08 09:41:01 -0700

    entry 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - integer goes in
  *
  * Return: Always return 0 (success)
- */ 
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -14,11 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
