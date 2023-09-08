ee7e5a0d3974b713b29d2ccc1af148451eb37e83
lupamo <arnoldlupamo@gmail.com>
2023-09-08 12:09:40 -0700

    base nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing nums
+ *
+ * Return: returns 0 after success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf('%d\n', n);
+	}
+	return (0);
+}
