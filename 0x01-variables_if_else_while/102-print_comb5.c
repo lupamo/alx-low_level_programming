c812b01b67a347d24145618feed97126ce086f66
lupamo <arnoldlupamo@gmail.com>
2023-09-10 05:18:30 -0700

    new try

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (int i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (int j = i + 1; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
