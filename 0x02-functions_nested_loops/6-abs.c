40416d75948ee7e7599daae5aeda648088b350e9
lupamo <arnoldlupamo@gmail.com>
2023-09-12 12:29:13 -0700

    task 6 try2

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int _abs - runs an integer
+ * @int: input inform of a number
+ * Return - returns 0
+ */
+int _abs(int)
+{
+	if (int < 0)
+		return (-n);
+	return (n);
+}	
