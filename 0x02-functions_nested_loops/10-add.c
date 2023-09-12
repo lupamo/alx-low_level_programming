7dda6e5cbcda29dbe55d293c870d1e9ad608995b
lupamo <arnoldlupamo@gmail.com>
2023-09-12 14:12:03 -0700

    new

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - prints num plus num
+ * add - prints num plus num
  * @n: input 1
  * @m: input 2
  * Return: two digit added
@@ -8,5 +8,6 @@
 int add(int n, int m)
 {
 	int summing = n + m;
+	
 	return (summing);
 }
