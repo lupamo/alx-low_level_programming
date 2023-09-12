96b5605cc909b75090f1300520ecab69a5325961
lupamo <arnoldlupamo@gmail.com>
2023-09-12 13:41:53 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * times_table - prints time table
+ * 
+ * Return: 0 success
+ */
+void times_table(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%2d, ", i * j);
+		}
+		_putchar("\n");
+	}
+}
