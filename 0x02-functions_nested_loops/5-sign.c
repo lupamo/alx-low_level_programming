1ea9a048d47aa7c8d04b0c0f189ce7070ba48f11
lupamo <arnoldlupamo@gmail.com>
2023-09-12 12:16:14 -0700

    task 5 try1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - a random digit
+ * @n: an input for number
+ * Returns: 1 or 0 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_purchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
