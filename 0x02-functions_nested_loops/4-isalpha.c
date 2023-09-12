2bba9179b966987ce4688aa6bdffd47f9fac2e4c
lupamo <arnoldlupamo@gmail.com>
2023-09-12 11:42:47 -0700

    task 4 try1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/**
+ * _isalpha - checks if alphabet is in caps
+ * @c: input to be checked
+ * Return: returns 0 if success
+ */
+int _isalpha(int c)
+{
+	char c;
+
+	for (c >= 'a'; c <= 'z'; c++)
+	{
+		if(c >= 'A'; c <= 'Z'; c++)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
