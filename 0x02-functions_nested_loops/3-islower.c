5e20b3a0fdefaf2c5949627bdc3ff9bbbb99d453
lupamo <arnoldlupamo@gmail.com>
2023-09-12 11:23:24 -0700

    task 3 try1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  _islower - prints 1 if char is lower
+ *
+ *  Return: returns 0
+ */
+
+int _islower(int c)
+{
+	if (c.islower() == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
