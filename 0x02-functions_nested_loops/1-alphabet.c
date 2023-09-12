94c6e29ddbfeac895117b9e2ddfb81b5f77404ff
lupamo <arnoldlupamo@gmail.com>
2023-09-12 10:07:16 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ *
+ * Return: 0 after executing
+ */
+
+int main(void)
+{
+	char i = 'a';
+	while(i < 'z'; i++)
+	{
+		_putchar("%d\n", i);
+		return (0);
+	}
+
+}
