a0f8319fe52b7df3b625fb1f54ee4aebd79cc3b2
lupamo <arnoldlupamo@gmail.com>
2023-09-12 14:09:46 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints num plus num
+ * @n: input 1
+ * @m: input 2
+ * Return: two digit added
+ */
+int add(int n, int m)
+{
+	int summing = n + m;
+	return (summing);
+}
