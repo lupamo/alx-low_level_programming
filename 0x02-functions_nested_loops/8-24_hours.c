8e7d2c40564ba7a1ae7554d8ea466e9b7f423851
lupamo <arnoldlupamo@gmail.com>
2023-09-12 13:12:48 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,10 +6,15 @@
  */
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
-		if (int minute = 0; minute < 60; minute++)
+	for (int hour = 0; hour <= 23; hour++)
+		if (int minute = 0; minute <= 59; minute++)
 		{
-			_putchar("%02d:%02d\n",hour, minute);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	return (0);
 }
