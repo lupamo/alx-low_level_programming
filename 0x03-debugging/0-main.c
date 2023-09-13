b1cee8c98b2e9bf02d8cb519f7574fffd9358aec
lupamo <arnoldlupamo@gmail.com>
2023-09-13 10:55:35 -0700

    testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - test if a number is positive
+ *
+ * Return: return 0 success
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
