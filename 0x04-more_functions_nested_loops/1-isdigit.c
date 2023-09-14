c0d24ba0b1fb366d9417b0c902185cb8487c4aad
lupamo <arnoldlupamo@gmail.com>
2023-09-14 10:57:22 -0700

    is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,12 @@
 /**
  * _isdigit - checks a number is digit
  * @c: parameter of digit
- * Return: one if true 0 if false
+ * Return: 1 if true 0 if false
  */
 int _isdigit(int c);
 {
 	if ( c >= 48 && c <= 57)
-	{
 		return (1);
-	}
 	else
-	{
-		return (0);
-	}
+		return (0);	
 }
