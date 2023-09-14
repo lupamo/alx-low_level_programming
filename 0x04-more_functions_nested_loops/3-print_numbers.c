16b1e9e46b68de1cc1782bbf2f7926953969ee52
lupamo <arnoldlupamo@gmail.com>
2023-09-14 11:31:28 -0700

    prints digitd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints num from 1 to 9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
