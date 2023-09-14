75eeeadcef0e9b5434901af486ae24f4dd94fe73
lupamo <arnoldlupamo@gmail.com>
2023-09-14 12:27:32 -0700

    nan

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers 0-9
+* Return: nan
+*/
+void print_numbers(void)
+{
+	char l;
+
+	for (l = '0'; l <= '9'; l++)
+	{
+	_putchar(l);
+	}
+	_putchar('\n');
+}
