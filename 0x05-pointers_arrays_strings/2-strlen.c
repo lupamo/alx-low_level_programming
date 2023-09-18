8863685f0a2ce4f77f1948128dcc5a18f4f5669f
lupamo <arnoldlupamo@gmail.com>
2023-09-18 12:32:45 -0700

    return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _strlen - returns length of string
+ * @s: is the string to be checked
+ * return: returns sting length
+ */
+int _strlen(char *s)
+{
+	for (i = 0; i <= sizeof(*s); i++)
+	{
+		return(i);
+	}
+}
