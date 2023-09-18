9d95c19f1d7d01945809741291e88a303d77f03e
lupamo <arnoldlupamo@gmail.com>
2023-09-18 14:30:39 -0700

    prints an arry

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - i)
-		{
-			printf(", ");
-		}
+		if (i < n - 1)
+		printf(", ");
 	}
 	printf("\n");
 }
