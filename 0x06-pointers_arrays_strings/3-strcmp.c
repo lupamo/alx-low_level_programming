b8d18144bc0f4623c694c8f9a7b4d369512361e5
lupamo <arnoldlupamo@gmail.com>
2023-09-20 12:09:52 -0700

    how to copy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			return ((*s1 > *s2) ? : -1);
-		}
 		s1++;
 		s2++;
 	}
-	if (*s1 == '\0' && *s2 != '\0')
+	if (*s1 < *s2)
 	{
 		return (-1);
 	}
-	else if (*s1 != '\0' && *s2 == '\0')
+	else if (*s1 > *s2)
 	{
 		return (1);
 	}
