56b55d95b4b262115e982489fea85ae7484f0734
lupamo <arnoldlupamo@gmail.com>
2023-09-20 11:42:05 -0700

    how to copy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * _strcmp - to compare two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: a comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return ((*s1 > *s2) ? : -1);
+		}
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 != '\0')
+	{
+		return (-1);
+	}
+	else if (*s1 != '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
