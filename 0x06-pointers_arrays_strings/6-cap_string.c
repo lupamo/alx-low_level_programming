91506832c3f5cb71989a25fd7738f1fd3f597055
lupamo <arnoldlupamo@gmail.com>
2023-09-21 11:37:01 -0700

    super

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,8 +14,7 @@ char *cap_string(char *str)
 		{
 			i++;
 		}
-		if (
-		str[i] == ' ' ||
+		if (str[i] == ' ' ||
 		str[i] == '\t' ||
 		str[i] == '\n' ||
 		str[i] == ',' ||
@@ -29,9 +28,7 @@ char *cap_string(char *str)
 		str[i] == '{' ||
 		str[i] == '}' ||
 		i == 0)
-		{
-		str[i] -= 32;
-		}
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
