07564bc342f4bdea1037746869625608a7cfacc2
lupamo <arnoldlupamo@gmail.com>
2023-09-20 17:14:03 -0700

    how to compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 
 	j = 0;
 
-	while (s1[j] != '\0' && s2[j] != '\0' && s1 == s2)
+	while (s1[j] != '\0' && s2[j] != '\0')
 	{
 		if (s1[j] != s2[j])
 		{
-			return (s1[j] != s2[j]);
+			return (s1[j] - s2[j]);
 		}
 		j++;
 	}
