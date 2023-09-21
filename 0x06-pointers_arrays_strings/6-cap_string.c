2771947dfb1b39f7a6f23b56a1fe425ac0943db3
lupamo <arnoldlupamo@gmail.com>
2023-09-21 11:31:35 -0700

    new try

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,26 +13,26 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
 			i++;
-			if (
-			str[i] == ' ' ||
-			str[i] == '\t' ||
-			str[i] == '\n' ||
-			str[i] == ',' ||
-			str[i] == ';' ||
-			str[i] == '.' ||
-			str[i] == '!' ||
-			str[i] == '?' ||
-			str[i] == '"' ||
-			str[i] == '(' ||
-			str[i] == ')' ||
-			str[i] == '{' ||
-			str[i] == '}' ||
-			i == 0)
-			{
-				str[i] -= 32;
-			}
-			i++;
 		}
-		return (str);
+		if (
+		str[i] == ' ' ||
+		str[i] == '\t' ||
+		str[i] == '\n' ||
+		str[i] == ',' ||
+		str[i] == ';' ||
+		str[i] == '.' ||
+		str[i] == '!' ||
+		str[i] == '?' ||
+		str[i] == '"' ||
+		str[i] == '(' ||
+		str[i] == ')' ||
+		str[i] == '{' ||
+		str[i] == '}' ||
+		i == 0)
+		{
+		str[i] -= 32;
+		}
+		i++;
 	}
+	return (str);
 }
