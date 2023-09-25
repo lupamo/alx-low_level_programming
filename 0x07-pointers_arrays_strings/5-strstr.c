c1224fdf8d8ce92c4b1ae1cda4fcdf22f69081d9
lupamo <arnoldlupamo@gmail.com>
2023-09-25 12:54:26 -0700

    haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,21 +8,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (int n = 0; haystack[n] != '\0'; n++)
-	{
-		if (haystack[n] == needle[0])
-		{
-			int j = 1;
+	char *h, *s;
 
-			while (needle[j] != '\0' && haystack[n + j] == needle[j])
-			{
-				j++;
-			}
-			if (needle[j] == '\0')
-			{
-				return (&haystack[n]);
-			}
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		s = needle;
+
+		while (*s != '\0' && *h == *s)
+		{
+			h++;
+			s++;
 		}
+		if (*s == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
 	}
 	return (NULL);
 }
