695b13bf002a14958afb58ff9fa9da294b9f349c
lupamo <arnoldlupamo@gmail.com>
2023-09-25 14:02:20 -0700

    size3

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,5 +16,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[(size * i) + i];
 		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
-	printf("%d\n, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
