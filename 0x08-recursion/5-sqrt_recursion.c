3107a0c3a5b08a931f0c01ba6a48b28fabc383a0
lupamo <arnoldlupamo@gmail.com>
2023-09-27 13:08:50 -0700

    rooting up

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,28 +20,27 @@ int sqrt_first(int n, int half)
 		}
 		return (0 + sqrt_first(n, half + 1));
 	}
-	/**
-	 * _sqrt_recursion - sets asqrt of a number
-	 * @n: number to be sqrted
-	 * @half: number predicted to be sqrt
-	 * Return: sqrt after run
-	 */
-	int _sqrt_recursion(int n)
-	{
-		if (n < 0)
-		{
-			return (-1);
-		}
-
-		if (n == 0)
-		{
-			return (0);
-		}
-		if (n == 1)
-		{
-			return (1);
-		}
-		return (sqrt_first(n, 2));
-	}
-
 }
+/**
+ * _sqrt_recursion - sets asqrt of a number
+ * @n: number to be sqrted
+ * @half: number predicted to be sqrt
+ * Return: sqrt after run
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (sqrt_first(n, 2));
+}
+
