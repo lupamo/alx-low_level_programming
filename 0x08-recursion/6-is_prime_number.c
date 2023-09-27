7ef9fd43d80111e447c7db7a88b1d5aa3c0a513c
lupamo <arnoldlupamo@gmail.com>
2023-09-27 13:40:09 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * is_prime_number - checks if a value is prime
+ * @n: value to be checked
+ * @divise: used to divide int
+ * Return: 0 or 1 depending on solution
+ */
+int is_prime_number(int n, int divise)
+{
+	if (divise * divise > n)
+	{
+		return (1);
+	}
+	if (n % divise == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number(n, divise + 1));
+}
