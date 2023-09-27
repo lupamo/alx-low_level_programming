ae9cc9a82b8e477a4399265e95e2b52686f6097b
lupamo <arnoldlupamo@gmail.com>
2023-09-27 04:11:33 -0700

    a reversed string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be reverse printed
+ * return: nothing to be returned
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
