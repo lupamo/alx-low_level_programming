b5c372c134e5cf46bcc4fa73da799be08157b84f
lupamo <arnoldlupamo@gmail.com>
2023-09-27 06:19:46 -0700

    power man!

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _pow_recursion - prints a power of numbers
+ * @x: base number
+ * @y: power number
+ * Return: zero or nothing
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
