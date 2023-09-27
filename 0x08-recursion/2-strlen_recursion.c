664ca0deaf3e41edd196d5a26cb57d945b9888f1
lupamo <arnoldlupamo@gmail.com>
2023-09-27 05:16:18 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - finds length of string
+ * @s: string to be found
+ * Return: nothing except string lengt
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+	return (i);
+}
