73ca299c24453209dbe7547fcfbd150265482c65
lupamo <arnoldlupamo@gmail.com>
2023-09-27 09:21:44 -0700

    trying to power up2

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,12 +11,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	else if (y > 0)
+	else if (y < 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
+		return (1.0 / (_pow_recursion(x, - y)));
 	}
 	else
 	{
-		return (1.0 / (_pow_recursion(x, -y)));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
