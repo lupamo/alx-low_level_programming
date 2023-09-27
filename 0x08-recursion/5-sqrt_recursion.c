3fa059f1ab74e37b750e64c212c62b25adc97ac3
lupamo <arnoldlupamo@gmail.com>
2023-09-27 09:47:28 -0700

    squaring root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * _sqrt_recursion - sets asqrt of a number
+ * @n: number to be sqrted
+ * Return: sqrt after run
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int approx = (n / 2);
+		int next_approx = (approx + (n / approx) / 2);
+
+		if (next_approx != approx)
+		{
+			approx = next_approx;
+			next_approx = (approx + (n / approx) / 2);
+		}
+		return (approx);
+	}
+}
