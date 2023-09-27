bd67dbaf067a83ab6bbea8e9d946933cbd56fe09
lupamo <arnoldlupamo@gmail.com>
2023-09-27 14:08:11 -0700

    reverse sam as foward

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * is_palindrome - checks if word is unreversible
+ * @s: unrevesible word
+ * @begin: start of word
+ * @end: last index of word
+ * Return: 1 if palindrom 0 if not
+ */
+int is_palindrome(char *s, int begin, int end)
+{
+	if (begin > end)
+	{
+		return (1);
+	}
+	if (s[begin] != s[end])
+	{
+		return (0);
+	}
+	return (is_palindrome(s, begin + 1, end - 1));
+}
