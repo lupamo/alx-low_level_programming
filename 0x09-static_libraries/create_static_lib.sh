dc984d3a7387e89a2afee3a4c157a7829fb560a8
lupamo <arnoldlupamo@gmail.com>
2023-09-29 07:29:14 -0700

    static library objects1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Get a list of all the .c files in the current directory.
-c_files=$(find . -type f -name "*.c")
-
-# Create a static library from the .c files.
-ar rcs libmy.a $c_files
-
-# Exit the script.
-exit 0
+temp_dir=$(mktemp -d)
+find . -type f -name "*.c" -exec gcc -c {} -o "$temp_dir/{}.o" \;
+ar rcs liball.a "$temp_dir"/*.o
+rm -rf "$temp_dir"
