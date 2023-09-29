6a6832a55bbeb5d43bcfb158b5dc41f4b53bad77
lupamo <arnoldlupamo@gmail.com>
2023-09-29 12:49:30 -0700

    printing command nsme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints out its program
+ * @argc: program commands
+ * @argv: program array commands
+ * Return: o if successful
+ */
+int main(int argc, char *argv[])
+{
+	char *name_prgm = argv[0];
+
+	printf("%s\n", name_prgm);
+	return (0);
+}
