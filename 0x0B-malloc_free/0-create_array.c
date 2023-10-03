a9374053d24ab650bd98c893648b635502198fa0
lupamo <arnoldlupamo@gmail.com>
2023-10-03 07:15:47 -0700

    creating an array2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr = (char *)malloc(size * sizeof(char));
 
-	while (arr == NULL)
+	while (arr == NULL || arr == 0)
 	{
 		return (NULL);
 	}
