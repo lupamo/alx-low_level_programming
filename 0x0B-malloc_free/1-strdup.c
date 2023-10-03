34179b7150d06fe0daa76d092d4212e954b88457
lupamo <arnoldlupamo@gmail.com>
2023-10-03 08:21:45 -0700

    duplicating stringss2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,18 +9,27 @@
 char *_strdup(char *str)
 {
 	char *dupstr;
-	
+	int i = 0, k = 1;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	dupstr = malloc(strlen(str) + 1);
+	while (str[k])
+	{
+		k++;
+	}
+	dupstr = malloc((sizeof(char) * k) + 1);
 
 	if (dupstr == NULL)
 	{
 		return (NULL);
 	}
-	strcpy(dupstr, str);
-	free(dupstr);
+	while (i < k)
+	{
+		dupstr[i] = str[i];
+		i++;
+	}
+	dupstr[i] = '\0';
 	return (dupstr);
 }
