d33103c256945c687e5ae523e300f780350d4cba
lupamo <arnoldlupamo@gmail.com>
2023-10-03 07:38:55 -0700

    duplicating string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - returns a pointer to a new str
+ * @str: sring to be duplicated
+ * Return: str if success or NULL
+ */
+char *_strdup(char *str)
+{
+	char *dupstr;
+	size_t length = strlen(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	dupstr = (char *)malloc((length + 1) * sizeof(char));
+
+	if (dupstr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(dupstr, str);
+	return (dupstr);
+}
