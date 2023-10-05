87fa249928223ba76f6d3b2d36e6cdb9b3acbbc5
lupamo <arnoldlupamo@gmail.com>
2023-10-05 12:37:31 -0700

    array estimate

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _evall - gets an estimate
- * @x: number to estimate on
- * Return: value estimate\
- */
-int _evall(int x)
-{
-	x < 0 ? (x *= -1) : (x = x)
-		return (x);
-}
-/**
  * array_range - arranges array from min to max
  * @min: less value
  * @max: more value
@@ -33,7 +23,7 @@ int *array_range(int min, int max)
 	}
 	for (i = 0; i < estm; min++, i++)
 	{
-		pnt[i] =min;
+		pnt[i] = min;
 	}
 	return (pnt);
 }
