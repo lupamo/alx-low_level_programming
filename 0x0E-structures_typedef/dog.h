5a1aae7731dd399ce01fc1236105d6a814c9688d
lupamo <arnoldlupamo@gmail.com>
2023-10-09 11:58:35 -0700

    struct function like5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog - a dog struct
+ * struct dog - a dog struct
  * @name: name of the dog
  * @age: age of the dog
  * @owner: the dog owner
- * Description: the dog struct contains dog character
+ * Description: the dog struct prints its character
  */
 struct dog
 {
