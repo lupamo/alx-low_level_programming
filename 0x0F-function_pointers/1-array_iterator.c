2bb09fab7d07dcd9cb0374618080c53691df0e49
lupamo <arnoldlupamo@gmail.com>
2023-10-11 10:46:08 -0700

    function that iterates another2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int j = 0;
 
-	if (array != NULL & action != NULL && size > 0)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		while (j < size)
 		{
