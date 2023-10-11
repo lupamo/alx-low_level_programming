1f40fc2b4a016d7244644fb3fe821cca73699cce
lupamo <arnoldlupamo@gmail.com>
2023-10-11 10:40:06 -0700

    function that iterates another

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * array_iterator - a function that iterates using another function
+ * @array: an array t be iterated
+ * @size: size of iterated arrat
+ * @action: array function iterator
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int j = 0;
+
+	if (array != NULL & action != NULL && size > 0)
+	{
+		while (j < size)
+		{
+			action(array[j]);
+			j++;
+		}
+	}
+}
