a60726fa0563114f87e35d6ff8569dbc530bed29
lupamo <arnoldlupamo@gmail.com>
2023-10-12 10:10:16 -0700

    summed up args

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - sums up all available arguments
+ * @n: first argument
+ * Return: summed up value
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i = 0;
+
+	va_start(ap, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		i += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (i);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Learning how to create a function with indefinite arguments in C
