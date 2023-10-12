606fa28f14b1651f5d593da1070d8ae55bdd5940
lupamo <arnoldlupamo@gmail.com>
2023-10-12 11:00:38 -0700

    printing arguments with separators

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints the number of ints in functions
+ * @separator: is the string to be printed between numbers
+ * @n: number of int in a function
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int j;
+
+	va_start(args, n);
+	if (n == 0)
+	{
+		return;
+	}
+	printf("%d", va_arg(args, int));
+	for (j = 1; j < n; j++)
+	{
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(args, int));
+	}
+	va_end(args);
+	printf("\n");
+}
