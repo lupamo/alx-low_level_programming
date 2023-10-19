d4ecfea907fccbeb00fd9f2b7ce92d7107839604
lupamo <arnoldlupamo@gmail.com>
2023-10-19 13:26:28 -0700

    reoved

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,10 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
+
 		h = h->next;
 		num_nodes++;
 	}
