e54710912df9d2817b5bf9782c63d1cb0092ba1f
lupamo <arnoldlupamo@gmail.com>
2023-10-26 07:15:03 -0700

    converting a bit too int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_to_uint - convertes binary to interger
+ * @b: points to 0 and 1 chars
+ *
+ * Return: an integer converted
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int count;
+	int i;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			count <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			count <<= 1;
+			count != 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (count);
+}
