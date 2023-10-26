8a0a81d85e304e47e5d8c03187aa42e31565d58d
lupamo <arnoldlupamo@gmail.com>
2023-10-26 10:19:00 -0700

    flipping bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * flip_bits - changes 1 and 0 to get to another
+ * @n: binary to be changed
+ * @m: binary to change to
+ * Return: the number of bits to be changed
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num;
+
+	num = 0;
+	while (n != m)
+	{
+		if ((n ^ m) & 1)
+		{
+			n >>= 1;
+			m >>= 1;
+		}
+		return (num);
+	}
+}
