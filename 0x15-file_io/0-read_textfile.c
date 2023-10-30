e9939f9c4ae9695c23d5048d8df85f505fcdc59d
lupamo_Arnold <arnoldlupamo@gmail.com>
2023-10-30 18:52:01 +0300

    reading file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 /**
  * read_textfile - reads a text file and prints it
  * @filename: name of file to be read
@@ -9,33 +10,25 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file = fopen(filename, "r");
-	char *buf = malloc(letters);
-	ssize_t byteRead = fread(buf, 1, letters, file);
-	ssize_t byteWrote = write(STDIN_FILENO, buf, byteRead);
-
-	if (file == NULL)
-	{
-		return (0);
-	}
-	if (buf == NULL)
-	{
-		fclose(file);
-		return (0);
-	}
-	if (byteRead < 0)
-	{
-		free(buf);
-		fclose(file);
-		return (0);
-	}
-	if (byteWrote < 0 || (size_t)byteWrote != (size_t)byteRead)
-	{
-		free(buf);
-		fclose(file);
-		return (0);
-	}
-	free(buf);
-	fclose(file);
-	return (byteRead);
+	int ad;
+    ssize_t byte_read;
+    char buff[letters];
+    
+    {
+        return (0);
+    }
+    ad = open(filename, O_RDONLY);
+    if (ad == -1)
+    {
+        return (0);
+    }
+    byte_read = read(ad, buff, letters);
+    if (byte_read == -1 || byte_read != letters)
+    {
+        close(ad);
+        return (0);
+    }
+    write(STDOUT_FILENO, buff, letters);
+    close(ad);
+    return (byte_read);
 }
