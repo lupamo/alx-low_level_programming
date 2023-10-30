744d3777a2b2bd8ebbc816352a0a6a9e47a7ba40
lupamo <arnoldlupamo@gmail.com>
2023-10-30 12:02:32 -0700

    printing and creating file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,6 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t text_len;
-	ssize_t byte_wrote;
 	int file_descriptor;
 
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -20,15 +18,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (file_descriptor == -1)
+	if (text_content)
 	{
-		text_len = strlen(text_content);
-		byte_wrote = write(file_descriptor, text_content, text_len);
-		if (byte_wrote != text_len)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
+		write(file_descriptor, text_content, strlen(text_content));
 	}
 	close(file_descriptor);
 	return (1);
