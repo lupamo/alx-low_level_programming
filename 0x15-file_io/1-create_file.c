d00139ecc57291e1ad6a8ef57fec797d9a9cbd32
lupamo <arnoldlupamo@gmail.com>
2023-10-30 11:39:46 -0700

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * create_file - creates a file
+ * @filename: is the file to create
+ * @text_content: file content
+ * Return: 1 on success or -1 on fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t text_len;
+	ssize_t byte_wrote;
+	int file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0700);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (file_descriptor)
+	{
+		text_len = strlen(text_content);
+		byte_wrote = write(file_descriptor, text_content, text_len);
+		if (byte_wrote != text_len)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+	close(file_descriptor);
+	return (1);
+}
