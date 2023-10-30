45800e398649750ef64fbd6a2a93152871b44f6e
lupamo <arnoldlupamo@gmail.com>
2023-10-30 09:14:26 -0700

    print out

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,24 +11,33 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int ad;
-    ssize_t byte_read;
-    char buff[letters];
-    
-    {
-        return (0);
-    }
-    ad = open(filename, O_RDONLY);
-    if (ad == -1)
-    {
-        return (0);
-    }
-    byte_read = read(ad, buff, letters);
-    if (byte_read == -1 || byte_read != letters)
-    {
-        close(ad);
-        return (0);
-    }
-    write(STDOUT_FILENO, buff, letters);
-    close(ad);
-    return (byte_read);
+	ssize_t byte_read;
+	char *buff;
+
+	buff = malloc(letters);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	ad = open(filename, O_RDONLY);
+	if (ad == -1)
+	{
+		return (0);
+	}
+	if (buff == NULL)
+	{
+		close(ad);
+		return (0);
+	}
+	byte_read = read(ad, buff, letters);
+	if (byte_read == -1 || byte_read != (signed int) letters)
+	{
+		close(ad);
+		free(buff);
+		return (0);
+	}
+	write(STDOUT_FILENO, buff, letters);
+	close(ad);
+	free(buff);
+	return (byte_read);
 }
