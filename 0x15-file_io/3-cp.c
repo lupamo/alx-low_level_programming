adb08e0acfda74a8223266df920d4f54217ffc14
lupamo <arnoldlupamo@gmail.com>
2023-10-31 02:50:20 -0700

    appender

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,48 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * file_append - copies a file to another
+ * @fd_from: file descriptor from main file
+ * @fd_to: file desccriptor to another file
+ * Return: nothing
+ */
+void file_append(const char *fd_from, const char *fd_to)
+{
+	int char1, char2, wrote;
+	char buff[1024];
+
+	char1 = open(fd_from, O_RDONLY);
+	if (fd_from || char1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd_from);
+		exit(98);
+	}
+	char2 = open(fd_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((wrote =  read(char1, buff, 1024)) > 0)
+	{
+		if (write(char2, buff, wrote) != wrote || char2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fd_to);
+			exit(99);
+		}
+	}
+	if (wrote == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd_from);
+		exit(98);
+	}
+	if (close(char1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", char1);
+		exit(100);
+	}
+	if (close(char2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", char2);
+		exit(100);
+	}
+}
+/**
  * main - checks arguments in command and appends
  * @argc: arguments in command
  * @argv: count of arguments
@@ -12,44 +54,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from;
-	int fd_to;
-
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0666);
-	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	char buffer[BUFFER_SIZE];
-	ssize_t byte_read;
-
-	while ((byte_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		if (write(fd_to, buffer, byte_read) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-	return (0);
+	file_append(argv[1], argv[2]);
+	exit(0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void file_append(const char *fd_from, const char *fd_to);
 #endif
