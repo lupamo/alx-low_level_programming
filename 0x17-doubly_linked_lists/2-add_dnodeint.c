998aaf7a90772ce1a819d7e99cf0c0c8df946788
lupamo <arnoldlupamo@gmail.com>
2023-12-07 14:26:42 +0300

    head node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,17 +9,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = *head;
-		*head = new_node;
-
-		return (&new_node);
-	}
-	else
+	if (!new_node)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (new_node);
 }
