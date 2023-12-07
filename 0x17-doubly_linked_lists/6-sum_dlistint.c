f56b9eaefeec2f0a4fb77722947d3299dba7103b
lupamo <arnoldlupamo@gmail.com>
2023-12-07 20:05:17 +0300

    adding list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - adds item value
+ * @head: first node element
+ * Return: total sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int count = 0;
+	dlistint_t *current = head;
+
+	if (!head)
+	{
+		return (0);
+	}
+	while (current)
+	{
+		count += current->n;
+		current = current->next;
+	}	
+	return (count);	
+}
