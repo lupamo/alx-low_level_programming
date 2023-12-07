b85728f66a9008cfcd8b9a88d5f91d866997477c
lupamo <arnoldlupamo@gmail.com>
2023-12-07 20:47:20 +0300

    number

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -46,6 +46,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (top->next == NULL)
 			top->next->prev = nodeNew;
 		top->next = nodeNew;
-		return (nodeNew);
 	}
+	return (nodeNew);
 }
