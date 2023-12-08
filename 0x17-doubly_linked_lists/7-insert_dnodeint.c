5570e85494d9555e16af8f14095bf1ee8f80d78f
lupamo <arnoldlupamo@gmail.com>
2023-12-08 05:33:18 +0300

    new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,40 +12,47 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *top;
 	unsigned int j;
 
-	nodeNew = malloc(sizeof(dlistint_t));
-	if (!*h || idx == 0)
+	if (!h)
 	{
-		if (nodeNew == NULL)
-		{
+		return (0);
+	}
+	nodeNew = malloc(sizeof(dlistint_t));
+
+	if (!nodeNew)
+	{
 			return (0);
-		}
-		nodeNew->n = n;
-		nodeNew->prev = NULL;
-		nodeNew->next = *h;
-		if (*h != NULL)
-		{
-			(*h)->prev = nodeNew;
-		}
+	}
+	nodeNew->n = n;
+	if (!(*h) && idx == 0)
+	{
 		*h = nodeNew;
 		return (nodeNew);
 	}
+	top = *h;
+	while (top->next && j < idx)
+	{
+		top = top->next;
+		j++;
+	}
+	if (j == idx)
+	{
+		nodeNew->next = top;
+		if (top->prev)
+			top->prev->next = nodeNew;
+		else
+			*h = nodeNew;
+		nodeNew->prev = top->prev;
+		top->prev = nodeNew;
+	}
+	else if (idx == j + 1)
+	{
+		top->next = nodeNew;
+		nodeNew->prev = top;
+	}
 	else
 	{
-		top = *h;
-		for (j = 0; j < idx - 1 && top != NULL; j++)
-		{
-			top = top->next;
-		}
-		if (top == NULL || j != idx - 1)
-		{
-			return (0);
-		}
-		nodeNew->n = n;
-		nodeNew->next = top->next;
-		nodeNew->prev = top;
-		if (top->next == NULL)
-			top->next->prev = nodeNew;
-		top->next = nodeNew;
+		free(nodeNew);
+		return(0);
 	}
 	return (nodeNew);
 }
