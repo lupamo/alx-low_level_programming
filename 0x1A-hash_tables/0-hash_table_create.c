28d2c621d3258dd1f2b7b8457d196ff8d155e031
lupamo <arnoldlupamo@gmail.com>
2023-12-22 15:26:42 +0300

    creatting hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- a function that creates a hash table
+ * @size: size ofthe array
+ * Return: the table if found
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t = malloc(sizeof(hash_table_t));
+
+	if (hash_t == NULL)
+	{
+		return (NULL);
+	}
+	hash_t->size = size;
+	hash_t->array = calloc(size, sizeof(void *));
+
+	if (hash_t->array == NULL)
+	{
+		free(hash_t);
+		return (NULL);
+	}
+
+	return (hash_t);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+#learning Hash maps and Hash Functions
