019d12baf47459a8e238bb6cd87bc22e7edb8f40
lupamo <arnoldlupamo@gmail.com>
2023-12-22 16:08:23 +0300

    malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,17 +10,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t = malloc(sizeof(hash_table_t));
+	unsigned long int j;
+
 
 	if (hash_t == NULL)
 	{
 		return (NULL);
 	}
-	hash_t->size = size;
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_t->array == NULL)
 	{
-			return (NULL);
+		free(hash_t);
+		return (NULL);
+	}
+	for (j = 0; j < size; j++)
+	{
+		hash_t->array[j] = NULL;
 	}
 
 	return (hash_t);
