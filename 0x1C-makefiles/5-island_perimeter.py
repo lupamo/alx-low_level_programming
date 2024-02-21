42d6d4b42565d841035067ea52fe1b65260fe724
lupamo <arnoldlupamo@gmail.com>
2024-02-22 01:50:32 +0300

    make file clean

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+	"""
+ 		function that returns the perimeter of an island
+		matrix manipulation
+ 	"""
+	pm = 0
+
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			if grid[i][j] == 1:
+				pm += 4
+
+				if i > 0 and grid[i - 1][j] == 1:
+					pm -= 2
+
+				if j > 0 and grid[i][j - 1] == 1:
+					pm -= 2
+	return pm
+
