2b56d9165a4d305fb889e9a95c83a4393fd80534
lupamo <arnoldlupamo@gmail.com>
2024-02-22 02:16:20 +0300

    grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """island perimeter function"""
 
+
 def island_perimeter(grid):
 	"""
  		function that returns the perimeter of an island
-		matrix manipulation
+		grid matrix manipulation of 0's and 1's
  	"""
 	pm = 0
 
 	for i in range(len(grid)):
 		for j in range(len(grid[0])):
 			if grid[i][j] == 1:
+				"""Cells of land is equal to 4"""
 				pm += 4
 
+				"""Water minus land cells"""
 				if i > 0 and grid[i - 1][j] == 1:
 					pm -= 2
 
